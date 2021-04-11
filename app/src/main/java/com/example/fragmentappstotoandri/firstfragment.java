e62750e05d16b8f0c79bb1a7e9efc4add8073bb8
totoandrigunawan <andritoto55@gmail.com>
2021-04-11 23:05:19 +0700

    projektugas4

---
+++ app/src/main/java/com/example/fragmentappstotoandri/firstfragment.java
@@ -0,0 +1,6 @@
+package com.example.fragmentappstotoandri;
+
+import android.app.Fragment;
+
+public class firstfragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/fragmentappstotoandri/secondactivity.java
@@ -0,0 +1,6 @@
+package com.example.fragmentappstotoandri;
+
+import android.app.Activity;
+
+public class secondactivity extends Activity {
+}
