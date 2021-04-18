25031ca9ad327403b12420437136d2f7f901e910
totoandrigunawan <andritoto55@gmail.com>
2021-04-18 17:08:51 +0700

    tugasprojek5

--- app/src/main/java/com/example/fragmentappstotoandri/fragmentactivity.java
+++ app/src/main/java/com/example/fragmentappstotoandri/fragmentactivity.java
@@ -1,4 +1,31 @@
 package com.example.fragmentappstotoandri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class fragmentactivity {
+import androidx.appcompat.app.AppCompatActivity;
+public class fragmentactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.action_seting){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
